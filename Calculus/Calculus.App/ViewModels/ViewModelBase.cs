06f2d9debb06604dbe07cb234a5f49fa55c68ca4
Andreev Artem <andreev_3731@mail.ru>
2022-05-17 16:38:02 +0300

    chore: add avalonia ui template project

---
+++ Calculus/Calculus.App/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace Calculus.App.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
---
+++ Calculus/Calculus.App/Views/Calculator.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Calculus.App.Views;
+
+public partial class Calculator : UserControl
+{
+    public Calculator()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
