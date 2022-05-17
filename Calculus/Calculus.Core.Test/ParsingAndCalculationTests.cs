6bc7ecd1be0867db2ab05f4d21b69c4a626fdc12
Andreev Artem <andreev_3731@mail.ru>
2022-05-17 13:44:53 +0300

    chore: add nUnit core tests project

---
+++ Calculus/Calculus.Core.Test/ParsingAndCalculationTests.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace Calculus.Core.Test;
+
+public class ParsingAndCalculationTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
